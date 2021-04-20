b7bb1d145c8aa664eaa454c464239a2efae22a6d
Aleksey <nirall@mail.ru>
2021-04-20 23:21:30 +0500

    Test func for recursive passing of the routing tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+type RouteObjType = {
+  route: string;
+  title: string;
+  nodes: Array<RouteObjType>;
+}
+
+type FormattedRouteType = {
+  route: string;
+  title: string;
+}
+
+const routesExample = {
+  route: '/',
+  title: 'home',
+  nodes: [
+    {
+      route: 'posts/',
+      title: 'posts',
+      nodes: [
+        {
+          route: 'user/',
+          title: 'userPosts',
+          nodes: []
+        },
+        {
+          route: 'admin/',
+          title: 'adminPosts',
+          nodes: []
+        }
+      ]
+    },
+    {
+      route: 'images/',
+      title: 'images',
+      nodes: [
+        {
+          route: 'admin/',
+          title: 'adminImages',
+          nodes: []
+        }
+      ]
+    }
+  ]
+}
+
+const App = () => {
+  const resultRoutes: Array<FormattedRouteType> = [];
+  const createArr = (parentRoute: string, routeObj: RouteObjType): void => {
+      const { nodes, title, route } = routeObj;
+      resultRoutes.push({
+        title,
+        route: `${parentRoute}${route}`,
+      });
+
+      if (nodes.length !== 0) {
+        nodes.forEach((node) => createArr(`${parentRoute}${route}`, node))
+      }
+  }
+
+  createArr('', routesExample);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Routing test</h1>
+        {resultRoutes.map(({ route, title }) => <h3 key={route}>{`Route: ${route}___________Title: ${title}`}</h3>)}
       </header>
     </div>
   );
